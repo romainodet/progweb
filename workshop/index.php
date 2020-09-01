53670b0d4f0ed402b333b1886efc5cd0461564de
Romain ODET <romain.odet@lecole-ldlc.com>
2020-09-01 14:47:14 +0200

    First commit

--- workshop/index.php
+++ workshop/index.php
@@ -1,1 +1,29 @@
-<h1 style="margin:25%">Nothing more there... <br/>Vous êtes arrivé au bout de l'internet.</h1>
+<!DOCTYPE html>
+<html>
+<head>
+	<!--Import Google Icon Font-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--Import materialize.css-->
+	<link type="text/css" rel="stylesheet" href="static/css/materialize.min.css"/>
+
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+<?php require('header.php'); ?>
+<main>
+	<div class="row">
+		<div class="col sm12 l8" id="main">
+			<h1>Main</h1>
+		</div>
+		<div class="col sm12 l4" id="relative">
+			<?php require('relative.php'); ?>
+		</div>
+	</div>
+
+</main>
+<?php require('footer.php'); ?>
+<!--JavaScript at end of body for optimized loading-->
+<script type="text/javascript" src="static/js/materialize.js"></script>
+</body>
+</html>
---
+++ workshop/relative.php
@@ -0,0 +1,1 @@
+<h1>Relative</h1>

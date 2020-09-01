1720fa4a37605a5f67933238d5b47f4a59558dd8
Pierre-Gilles Levallois <pierregille.levallois@gmail.com>
2020-09-01 10:28:31 +0200

    plan du workshop

--- workshop/todo.php
+++ workshop/todo.php
@@ -1,17 +1,49 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.min.css">
 </head>
 <body>
-    <h4>Objectif : developper un blog from scratch</h4>
-    <ol>
-        <li>Découper une page html (header/content/footer)</li>
-        <li>Faire un menu de navigation entre les différentes rubriques du blog</li>
-        <li>Penser le modèle Mysql qui permet de stocker les données du blog.</li>
-        <li>Développer une page de liste d'articles</li>
-        <li>Développer une page de détails d'un article (visualisation d'un article)</li>
-        <li>Développer un systeme de commentaire, s'il nous reste du temps.</li>
+<div class="container">
+    <header>
+        <a href="#" class="logo">Workshop de Septembre 2020</a>
+        <a href="index.php" class="button">Index</a>
+        <a href="todo.php" class="button">Todo</a>
+    </header>
+  <div class="row">
+    <div class="col-sm-2"></div>
+    <div class="col-sm-8">
+        <div class="card fluid">
+            <h4><span class="icon-edit"></span>&nbsp;Objectif : developper un blog from scratch</h4>
+            <ol>
+                <li>Découper une page html (header/content/footer)</li>
+                <li>Faire un menu de navigation entre les différentes rubriques du blog</li>
+                <li>Penser le modèle Mysql qui permet de stocker les données du blog.</li>
+                <li>Développer une page de liste d'articles</li>
+                <li>Développer une page de détails d'un article (visualisation d'un article)</li>
+            </ol>
+        </div>
+    </div>
+    <div class="col-sm-2"></div>
+  </div>
+  <div class="row">
+  <div class="col-sm-2"></div>
+    <div class="col-sm-8">
+        <div class="card fluid">
 
-    </ol>
+            <h4><span class="icon-edit"></span>&nbsp;Pour aller plus loin...</h4>
+            <ol>
+                <li>Développer un système de commentaires</li>
+                <li>Mettre en place une authentification avec mySql</li>
+                <li>Utiliser les WebSockets</li>
+                <li>Syndiquer des contenus venant d'un autre site</li>
+            </ol>
+        </div>
+    </div>
+    <div class="col-sm-2"></div>
+</div>
+<footer class="sticky">
+  <p>LDLC - Cours Programmation Web PHP/MySql - Septembre 2020</p>
+</footer>
 </body>
 </html>
